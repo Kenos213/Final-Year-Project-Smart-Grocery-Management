4fefa74ac3ae61b822fc9bc0888b83384471d1b8
Alex <alex@Alexs-MacBook-Pro.local>
2026-01-20 12:10:57 +0000

    Intial Upload With Flask Back End Setup

---
+++ backend/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+CORS(app)
+
+@app.get("/health")
+def health():
+    return jsonify({"status": "ok"})
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8000 ,debug=True)
